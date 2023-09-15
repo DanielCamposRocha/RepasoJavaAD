ea3ddc8a88e174f72c1983857577cc83c854d031
Daniel <danielcamposrocha9@gmail.com>
2023-09-15 14:51:30 +0200

    6 casi listo revisar que no haya repetidos

--- src/main/java/ejercicios/Ejercicio101.java
+++ src/main/java/ejercicios/Ejercicio101.java
@@ -1,2 +1,20 @@
-package ejercicios;public class Ejercicio101 {
+package ejercicios;
+
+public class Ejercicio101 {
+    public static String texto = "4;5;7;8;4";
+    //sumar los numeros de un string separados por ;
+    public static void suma(int a){
+        int sumafinal=0;
+        String [] numeros=texto.split(";");
+        if(a==1){
+            for (String numero:numeros ) {
+                sumafinal=sumafinal+Integer.parseInt(numero);
+            }
+        }else {
+            for(int i=0;i< numeros.length;i++){
+                sumafinal=sumafinal+Integer.parseInt(numeros[i]);
+            }
+        }
+        System.out.println("La suma es : "+sumafinal);
+    }
 }
--- src/main/java/ejercicios/Ejercicio102.java
+++ src/main/java/ejercicios/Ejercicio102.java
@@ -1,2 +1,31 @@
-package ejercicios;public class Ejercicio102 {
+package ejercicios;
+
+import utilidades.Utilidades;
+
+//Realiza un programa en Java que pida por teclado el nombre y la edad de una persona mayor de edad (se debe validar el valor introducido) y responda indicando:
+//
+//    Si no está jubilado (edad de jubilación = 65 años), cuántos años le quedan para jubilarse.
+//    Si ya está jubilada indicará cuántos años lleva jubilado.
+public class Ejercicio102 {
+    public static final int EDAD_JUBILACION=65;
+    public static void calculoJubilacion(){
+        String nombre=Utilidades.pedirString("Introduzca su nombre");
+        int edadReal= validarEdad();
+        if(edadReal<65){
+            System.out.println(nombre+" tiene usted "+edadReal+" años, le faltan "+(EDAD_JUBILACION-edadReal)+" años para jubilarse");
+        }else{
+            System.out.println(nombre+" tiene usted "+edadReal+" años, lleva "+(edadReal-EDAD_JUBILACION)+" años disfrutando de la jubilación");
+        }
+    }
+
+    public static int validarEdad(){
+        int edadValidada=0;
+        do{
+            int edadAValidar= Utilidades.pedirInt("Introduzca su edad");
+            if (edadAValidar>=18 && edadAValidar<150){
+             edadValidada=edadAValidar;
+            }
+        }while(edadValidada==0);
+        return edadValidada;
+    }
 }
